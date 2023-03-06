4277cb2e87b98bef068ea4e08dccd933d1c272fc
Luis Zarza <luis@luiszarza.com>
2023-03-06 17:59:00 +0800

    create model

---
+++ db/migrate/20230306095811_create_activities.rb
@@ -0,0 +1,13 @@
+class CreateActivities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :activities do |t|
+      t.string :title
+      t.text :description
+      t.string :location
+      t.date :date
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
