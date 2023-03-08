5ae4112f4ba24895ad2c39bb6dd835f596695bae
Matt Krannig <mpkrannig@yahoo.com>
2023-03-07 23:26:37 -0500

    added handy relations between users and activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,8 @@
 class Activity < ApplicationRecord
+  # belongs_to :owner, class_name: "User", foreign_key: :user_id
   belongs_to :user
+  alias_attribute :owner, :user
   has_many_attached :photos
+  has_many :bookings
+  has_many :attendees, through: :bookings, source: :user
 end
