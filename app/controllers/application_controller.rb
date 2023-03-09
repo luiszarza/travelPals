f01c79260a61859b90894c564f8dcc031e90bcf8
Aidan Keenan <aidankeenan7137@gmail.com>
2023-03-09 15:17:48 +0800

    added the necessary validation to sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def configure_permitted_parameters
-  #   # For additional fields in app/views/devise/registrations/new.html.erb
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :photo])
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :age, :photo])
 
-  #   # For additional in app/views/devise/registrations/edit.html.erb
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :photo])
-  # end
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :bio, :age, :photo])
+  end
 
     private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   has_many :organized_activities, class_name: "Activity", foreign_key: :user_id
   has_many :bookings, dependent: :destroy
   has_many :attending_activities, through: :bookings, source: :activity
+  validates :name, presence: true
+  validates :bio, presence: true
+  validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 18 }
+  validates :photo, presence: true
 end
