66aa64d9cc863f685923c7cc8615301372bb2bd4
Luis Zarza <luis@luiszarza.com>
2023-03-15 16:00:12 +0800

    Advanced administrator2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
   has_many :organized_activities, class_name: "Activity", foreign_key: :user_id
   has_many :bookings, dependent: :destroy
   has_many :attending_activities, through: :bookings, source: :activity
-  # validates :name, presence: true
-  # validates :bio, presence: true
-  # validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 18 }
-  # validates :photo, presence: true
+  validates :name, presence: true
+  validates :bio, presence: true
+  validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 18 }
+  validates :photo, presence: true
   # has_many :chatrooms
   has_many :chatrooms_as_recipient, class_name: "Chatroom", foreign_key: :recipient_id
   has_many :comments, dependent: :destroy
