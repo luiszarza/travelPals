c093e266bb6af07930fdecf1ecd7728478457c75
Aidan Keenan <aidankeenan7137@gmail.com>
2023-03-15 10:51:38 +0800

    fixed empty chat error

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,7 +2,4 @@ class Chatroom < ApplicationRecord
   belongs_to :requester, class_name: "User", foreign_key: "user_id"
   belongs_to :recipient, class_name: "User"
   has_many :messages, dependent: :destroy
-  validates :messages, presence: true
 end
-
-private
