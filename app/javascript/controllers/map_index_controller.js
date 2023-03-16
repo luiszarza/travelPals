571d711c64fcc7e287875db94c16c81cc0723370
Aidan Keenan <aidankeenan7137@gmail.com>
2023-03-16 15:56:30 +0800

    map update: getting the user location

--- app/javascript/controllers/map_index_controller.js
+++ app/javascript/controllers/map_index_controller.js
@@ -7,7 +7,7 @@ export default class extends Controller {
     info: Object
   }
 
-  static targets = [ "box" ]
+  static targets = [ "box", "card" ]
 
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
@@ -32,6 +32,35 @@ export default class extends Controller {
     })
   }
 
+  #arrangeCards(coordinates) {
+    this.cardTargets.forEach((card) => {
+      const distance = this.#getDistanceFromLatLonInKm(coordinates.latitude, coordinates.longitude, card.dataset.lat, card.dataset.lng);
+      card.dataset.distance = distance;
+    })
+    const sortedCards = this.cardTargets.sort((a, b) => a.dataset.distance - b.dataset.distance)
+    sortedCards.forEach((card) => {
+      this.boxTarget.appendChild(card);
+    })
+  }
+
+  #getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = this.#deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = this.#deg2rad(lon2-lon1);
+    var a =
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(this.#deg2rad(lat1)) * Math.cos(this.#deg2rad(lat2)) *
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c; // Distance in km
+    return d;
+  }
+
+  #deg2rad(deg) {
+    return deg *(Math.PI/180)
+  }
+
   #fitMapToCenter(coordinates) {
     const bounds = new mapboxgl.LngLatBounds()
     if (coordinates === {}) {
@@ -46,6 +75,7 @@ export default class extends Controller {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           // this.map.setCenter([ position.coords.longitude,  position.coords.latitude]);
+          this.#arrangeCards(position.coords);
           this.#addMarkersToMap();
           this.#fitMapToCenter(position.coords);
         },
