b751adb73fa3df3655c1719cb16462fa765712b8
Aidan Keenan <aidankeenan7137@gmail.com>
2023-03-16 15:17:32 +0800

    map update: getting the user location

--- app/javascript/controllers/map_index_controller.js
+++ app/javascript/controllers/map_index_controller.js
@@ -19,8 +19,7 @@ export default class extends Controller {
     })
 
     console.log("Hello from new maps controller index");
-    this.#addMarkersToMap()
-    this.#fitMapToCenter()
+    this.#getUserLocation();
     // disable map zoom when using scroll
     this.map.scrollZoom.disable();
   }
@@ -33,14 +32,33 @@ export default class extends Controller {
     })
   }
 
-  #fitMapToCenter() {
-    // const bounds = new mapboxgl.LngLatBounds()
-    // bounds.extend([115.1470086, -8.6594826])
-    // this.map.fitBounds(bounds, { padding: 70, maxZoom: 10, duration: 3000 })
-
+  #fitMapToCenter(coordinates) {
     const bounds = new mapboxgl.LngLatBounds()
-    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+    if (coordinates === {}) {
+      bounds.extend([115.1470086, -8.6594826])
+    }
+    bounds.extend([ coordinates.longitude, coordinates.latitude ])
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 12, duration: 2000 })
   }
 
+  #getUserLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          // this.map.setCenter([ position.coords.longitude,  position.coords.latitude]);
+          this.#addMarkersToMap();
+          this.#fitMapToCenter(position.coords);
+        },
+        () => {
+          console.log('Unable to get user location');
+          this.#addMarkersToMap();
+          this.#fitMapToCenter({});
+        }
+      )
+    } else {
+      console.log('Geolocation is not supported by this browser.');
+      this.#addMarkersToMap();
+      this.#fitMapToCenter({});
+    }
+  }
 }
